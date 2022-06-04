46848be1f2fb9ba4350e20f99782652ba0515fc8
durrenlee <durrenli@sina.com>
2022-06-04 23:22:13 +0800

    add train function

--- rnn/sequence.py
+++ rnn/sequence.py
@@ -58,17 +58,34 @@ batch_size, n_train = 16, 600
 train_iter = d2l.load_array((features[:n_train], labels[:n_train]), batch_size, is_train=True)
 
 
-# MLP
+# Glorot initialization
 def init_weights(m):
     if type(m) == nn.Linear:
         nn.init.xavier_uniform_(m.weight)
 
 
+# MLP
 def get_net():
     # input layer: 4 units, hidden layer: 10 units, output: 1 (scalar)
-    net = nn.Sequential(nn.Linear(4, 10), nn.ReLU(), nn.Linear(10, 1))
-    net.apply(init_weights)
-    return net
+    mlp_net = nn.Sequential(nn.Linear(4, 10), nn.ReLU(), nn.Linear(10, 1))
+    mlp_net.apply(init_weights)
+    return mlp_net
 
 
+# MSE Loss, note that there is no 1/2
 loss = nn.MSELoss()
+
+
+def train(net, train_iter, loss, epochs, lr):
+    trainer = torch.optim.Adam(net.parameters(), lr)
+    for epoch in range(epochs):
+        for X, y in train_iter:
+            trainer.zero_grad()
+            l = loss(net(X), y)
+            l.backward()
+            trainer.step()
+        print(f'epoch{epoch + 1}, ', f'loss:{d2l.evaluate_loss(net, train_iter, loss):f}')
+
+
+net = get_net()
+train(net, train_iter, loss, 5, 0.01)
